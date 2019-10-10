c44854caaad04d2a801ccf86652ac69abfaf161a
saaashimi <kevin@saavedra.io>
2019-10-10 09:17:18 -0700

    fix checkout alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,7 +7,8 @@ Set-Alias -Name cdpy -Value cd_qapyunit
 function git_status { git status }
 Set-Alias -Name gst -Value git_status
 
-function git_checkout { git checkout }
+function git_checkout([string]$branchname) 
+{ git checkout $branchname }
 Set-Alias -Name gch -Value git_checkout
 
 function git_branchv { git branch -v }
@@ -17,19 +18,13 @@ function git_push { git push }
 Set-Alias -Name gpu -Value git_push
 
 function git_pushsetupstream([string]$branchname) 
-{
-   git push --set-upstream origin $branchname
-}
+{ git push --set-upstream origin $branchname }
 Set-Alias -Name gpusu -Value git_pushsetupstream
 
 function git_checkoutb([string]$branchname) 
-{
-    git checkout -b $branchname
-}
+{ git checkout -b $branchname }
 Set-Alias -Name gchb -Value git_checkoutb
 
 function git_add-u_comm([string]$message) 
-{
-    git add -u; git commit -m $message
-}
+{ git add -u; git commit -m $message }
 Set-Alias -Name gaducom -Value git_add-u_comm
