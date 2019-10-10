93e114196ee0d9a6405387e021239fbf1c05849f
saaashimi <kevin@saavedra.io>
2019-10-10 09:14:47 -0700

    Update profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,9 @@
-# Place in $PSHOME\Microsoft.PowerShell_profile.ps1
-
-# Directory alisases
 function cd_repos { cd C:\Users\kev10076\Repos }
 Set-Alias -Name cdre -Value cd_repos  
 
 function cd_qapyunit { cd C:\qatest\pyunit }
 Set-Alias -Name cdpy -Value cd_qapyunit
 
-# Github Alisases
 function git_status { git status }
 Set-Alias -Name gst -Value git_status
 
@@ -20,11 +16,17 @@ Set-Alias -Name gbv -Value git_branchv
 function git_push { git push }
 Set-Alias -Name gpu -Value git_push
 
+function git_pushsetupstream([string]$branchname) 
+{
+   git push --set-upstream origin $branchname
+}
+Set-Alias -Name gpusu -Value git_pushsetupstream
+
 function git_checkoutb([string]$branchname) 
 {
     git checkout -b $branchname
 }
-Set-Alias -Name gbv -Value gitcheckoutb
+Set-Alias -Name gchb -Value git_checkoutb
 
 function git_add-u_comm([string]$message) 
 {
